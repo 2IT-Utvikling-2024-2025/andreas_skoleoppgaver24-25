61f4c611c5a9a9f8cae0410b7ae468b813858fb9
Andreas Ski <miniskiandreas@icloud.com>
2024-09-11 08:41:18 +0200

    la til en ny oppgave med Joakim

---
+++ Oppgave4_bombe/src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Clock from './Clock.jsx'
+
+function App() {
+  
+  return (
+    <>
+      
+      <Clock />
+      
+    </>
+  )
+}
+
+export default App
